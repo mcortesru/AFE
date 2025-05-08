04d1cc8e3c2d99333e13349a00c07866360d9c94
ruizmaa <ruizma31@gmail.com>
2025-05-08 12:55:55 +0200

    Fixed Neo4j port to 7690

--- creador_de_grafos.py
+++ creador_de_grafos.py
@@ -8,7 +8,7 @@ from pathlib import Path
 # 🔧 Conexión a Neo4j
 # ==========================
 load_dotenv()
-NEO4J_URI = os.getenv("NEO4J_URI", "bolt://localhost:7687")
+NEO4J_URI = os.getenv("NEO4J_URI", "bolt://localhost:7690")
 NEO4J_USER = os.getenv("NEO4J_USER", "neo4j")
 NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
 
